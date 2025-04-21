c8f818ff9f8ee5341481a09bfa599b8d3cfc745d
Taiki <tacosuke12@gmail.com>
2025-04-21 16:34:41 +0900

    表示の修正

--- frontend/pages/summary.py
+++ frontend/pages/summary.py
@@ -12,33 +12,41 @@ from ai_agent.schemas import SummarySchema
 def show_result(summary_result: SummarySchema):
     try:
         st.subheader("論文情報")
-        st.text(f'English Title:\n{summary_result.English_Title}')
-        st.text(f'タイトル:\n{summary_result.Japanese_Title}')
-        st.text(f"発表年度:\n{str(summary_result.Publication_Year)}")
-        st.text(f"ジャーナル/カンファレンス:\n{summary_result.Journal_or_Conference}")
-        st.text(f"キーワード:\n{', '.join(summary_result.Keywords)}")
+        st.text('English Title:')
+        st.code(summary_result.English_Title, language='text')
+        st.text('タイトル:')
+        st.code(summary_result.Japanese_Title, language='text')
+        st.text("発表年度:")
+        st.code(str(summary_result.Publication_Year), language='text')
+        st.text("ジャーナル/カンファレンス:")
+        st.code(summary_result.Journal_or_Conference, language='text')
+        st.text("キーワード:")
+        st.code(', '.join(summary_result.Keywords), language='text')
 
         st.subheader("概要")
-        st.text(summary_result.Japanese_Abstract)
+        st.code(summary_result.Japanese_Abstract, language='text')
 
         st.subheader("提案手法")
-        st.text(summary_result.Summary_of_Proposed_Method)
+        st.code(summary_result.Summary_of_Proposed_Method, language='text')
 
         st.subheader("新規性")
-        st.text(summary_result.Summary_of_Novelty_of_Proposed_Method)
+        st.code(summary_result.Summary_of_Novelty_of_Proposed_Method, language='text')
 
         st.subheader("実験と結果")
-        st.text(summary_result.Summary_of_Experiments_and_Results)
+        st.code(summary_result.Summary_of_Experiments_and_Results, language='text')
 
         st.subheader("今後の展望")
-        st.text(summary_result.Summary_of_Future_Work)
+        st.code(summary_result.Summary_of_Future_Work, language='text')
 
         st.subheader("全体のまとめ")
-        st.text(summary_result.Overall_Summary)
+        st.code(summary_result.Overall_Summary, language='text')
 
         st.subheader("重要な参考文献")
+        reference_str = '[ \n'
         for ref in summary_result.Important_Reference:
-            st.markdown(f"- {ref}")
+            reference_str += f"    \"{ref}\",\n"
+        reference_str += "]"
+        st.code(reference_str, language='text')
     
     except Exception as e:
         raise e
--- frontend/pages/translate.py
+++ frontend/pages/translate.py
@@ -12,27 +12,31 @@ from ai_agent.schemas import TranslateSchema
 def show_result(translate_result: TranslateSchema):
     try:
         st.subheader("論文情報")
-        st.text(f'English Title:\n{translate_result.English_Title}')
-        st.text(f'タイトル:\n{translate_result.Japanese_Title}')
-        st.text(f"発表年度:\n{str(translate_result.Publication_Year)}")
-        st.text(f"ジャーナル/カンファレンス:\n{translate_result.Journal_or_Conference}")
-        st.text(f"キーワード:\n{', '.join(translate_result.Keywords)}")
+        st.text('English Title:')
+        st.code(translate_result.English_Title, language='text')
+        st.text('タイトル:')
+        st.code(translate_result.Japanese_Title, language='text')
+        st.text("発表年度:")
+        st.code(str(translate_result.Publication_Year), language='text')
+        st.text("ジャーナル/カンファレンス:")
+        st.code(translate_result.Journal_or_Conference, language='text')
+        st.text("キーワード:")
+        st.code(', '.join(translate_result.Keywords), language='text')
 
         st.subheader("概要")
-        st.text(translate_result.Abstract)
+        st.code(translate_result.Abstract, language='text')
 
         st.subheader("イントロダクション")
-        st.text(translate_result.Introduction)
+        st.code(translate_result.Introduction, language='text')
 
         st.subheader("関連研究")
-        st.text(translate_result.Related_Research)
+        st.code(translate_result.Related_Research, language='text')
 
         st.subheader("提案手法")
-        st.text(translate_result.Method)
+        st.code(translate_result.Method, language='text')
 
         st.subheader("まとめ")
-        st.text(translate_result.Conclusion)
-    
+        st.code(translate_result.Conclusion, language='text')
     except Exception as e:
         raise e
 
